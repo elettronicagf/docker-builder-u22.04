101b7bd46003a7281f0e31a5a2aedf97f5286477
Mauro Salvini <mauro.salvini@elettronicagf.it>
2025-10-02 09:20:28 +0200

    docker_run.sh: some improvements
    - allow to pass a different downloads directory
    - if SSTATE_MIRRORS_SRC is passed by the caller, bind mount local SSTATE mirror
      directory and create configuration to use it
    - if SITECONF_SRC is passed by the caller, bind mount local SITECONF directory
    
    Signed-off-by: Mauro Salvini <mauro.salvini@elettronicagf.it>

--- docker_run.sh
+++ docker_run.sh
@@ -7,13 +7,33 @@ DOCKER_IMAGE="egf-ubuntu-22.04:latest"
 # FOLDERS BINDING
 OUTPUT_SRC=${PROG_DIR}/yocto-output
 INPUT_SRC=${PROG_DIR}/yocto-input
-DOWNLOADS_SRC=${PROG_DIR}/downloads
+DOWNLOADS_SRC=${DOWNLOADS_SRC:-${PROG_DIR}/downloads}
 
 OUTPUT_DEST=/home/yocto/yocto-output
 INPUT_DEST=/home/yocto/yocto-input
 DOWNLOADS_DEST=/home/yocto/downloads
+SSTATE_MIRROR_DEST=/home/yocto/sstate-mirror
+SITENCONF_DEST=/home/yocto/.yocto
+
+SSTATE_MIRROR_BINDMOUNT=
+SSTATE_MIRROR_CREATECONFIG=
+SITECONF_BINDMOUNT=
+
+if [ -n $SSTATE_MIRRORS_SRC ]
+then
+       SSTATE_MIRROR_BINDMOUNT="--mount=type=bind,source=${SSTATE_MIRRORS_SRC},target=${SSTATE_MIRROR_DEST}"
+       SSTATE_MIRROR_ENABLE="/home/yocto/enable_sstate_mirror.sh ${SSTATE_MIRROR_DEST}"
+fi
+
+if [ -n $SITECONF_SRC ]
+then
+       SITECONF_BINDMOUNT="--mount=type=bind,source=${SITECONF_SRC},target=${SITENCONF_DEST}"
+fi
 
 docker run -h dockerbuilder --network=host -it --rm --security-opt seccomp=unconfined \
 --mount='type=bind,source='${OUTPUT_SRC}',target='${OUTPUT_DEST} \
 --mount='type=bind,source='${INPUT_SRC}',target='${INPUT_DEST} \
---mount='type=bind,source='${DOWNLOADS_SRC}',target='${DOWNLOADS_DEST} $DOCKER_IMAGE
+--mount='type=bind,source='${DOWNLOADS_SRC}',target='${DOWNLOADS_DEST} \
+$SSTATE_MIRROR_BINDMOUNT \
+$SITECONF_BINDMOUNT \
+$DOCKER_IMAGE $SSTATE_MIRROR_ENABLE
